4d404adb41ac1e19079f79f6cf413ac527ddc304
= <nestlerj@bouldercolorado.gov>
2020-01-28 12:36:56 -0700

    Add new class for cataloging impervious layers

--- impervious.py
+++ impervious.py
@@ -1,3 +1,5 @@
+import os
+import shelve
 import getpass
 import logging
 import logging.config
@@ -17,6 +19,55 @@ with open(r'.\impervious-surfaces\config.yaml') as config_file:
 read_conn = config['connections']['read']
 edit_conn = config['connections']['edit']
 
+
+# Create a class for impervious layers
+class Impervious:
+    def __init__(self, lyr):
+        self.name = list(lyr)[0]
+        self.location = os.path.join(edit_conn, "PW.PWAREA")
+        self.path = os.path.join(self.location, self.name)
+        self.query = "LIFECYCLE = 'Active'" + list(lyr)[1]
+        self.rows = self.rows()
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __key(self):
+        """Creates a tuple ordered by GLOBALIDs, used for hash comparisons."""
+
+        key = tuple(sorted(self.rows, key=lambda y: y[0]))
+        return key
+
+    def rows(self):
+        """Returns a list of tuples representing (GLOBALID, SHAPE) pairs."""
+
+        fields = ["GLOBALID", "SHAPE@"]
+        rows = []
+
+        with arcpy.da.SearchCursor(self.path, fields, self.query) as cursor:
+            for row in cursor:
+                rows.append((row[0], row[1]) for i in range(len(fields)))
+
+        return rows
+
+    def store_current(self):
+        """Stores the __key() of a table for hash comparisons."""
+        with shelve.open('.\\log\\previous_run', 'c') as db:
+            db[self.name] = self.__key()
+
+    def equals_previous(self):
+        """Compares the current table to the previous run to ID changes."""
+        try:
+            with shelve.open('.\\log\\previous_run', 'c') as db:
+                previous = db[self.name]
+            if hash(self) == hash(previous):
+                return True
+            else:
+                return False
+        except KeyError:
+            self.store_current()
+
+
 # Define order for intersecting layers, and relevant queries for each
 layers = [{"GISPROD3.PW.PWMaintenanceArea":
            "AND FACILITYTYPE = 'Median' AND SURFTYPE = 'Hard'"},
@@ -28,7 +79,5 @@ layers = [{"GISPROD3.PW.PWMaintenanceArea":
           {"GISPROD3.PW.ImperviousMisc": ""}]
 
 # Move through each layer
-for layer in sorted(layers, key=lambda x: layers[x]['order']):
-    query = "LIFECYCLE = 'Active'"
-    if layers[layer]['query']:
-        query += ' AND ' + layers[layer]['query']
+for layer in layers:
+    feature = Impervious(layer)
