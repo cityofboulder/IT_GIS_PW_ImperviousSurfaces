2247daacde9a751f4a8f3e9dfd8420f787cc0ea3
= <nestlerj@bouldercolorado.gov>
2020-01-28 11:37:37 -0700

    Organize data structure that defines layers

--- impervious.py
+++ impervious.py
@@ -14,8 +14,27 @@ with open(r'.\impervious-surfaces\config.yaml') as config_file:
     config = yaml.safe_load(config_file.read())
     logging.config.dictConfig(config['logging'])
 
-read_conn = config['connection']['read']
-edit_conn = config['connection']['edit']
+read_conn = config['connections']['read']
+edit_conn = config['connections']['edit']
 arcpy.env.workspace = read_conn
 
-fcs = arcpy.ListFeatureClasses(feature_dataset='PW.PWAREA')
+# Define order for intersecting layers, and relevant queries for each
+layers = {'GISPROD3.PW.PWMaintenanceArea': {
+    'order': 0,
+    'query': "FACILITYTYPE = 'Median' AND SURFTYPE = 'Hard'"
+        },
+    'GISPROD3.PW.Building': {'order': 1, 'query': ''},
+    'GISPROD3.PW.RoadArea': {'order': 2, 'query': ''},
+    'GISPROD3.PW.ParkingLot': {
+    'order': 3, 'query': "SURFACETYPE = 'Impervious'"
+        },
+    'GISPROD3.PW.Driveway': {'order': 4, 'query': ''},
+    'GISPROD3.PW.SidewalkArea': {'order': 5, 'query': ''},
+    'GISPROD3.PW.ImperviousMisc': {'order': 6, 'query': ''}
+}
+
+# Move through each layer
+for layer in sorted(layers, key=lambda x: layers[x]['order']):
+    query = "LIFECYCLE = 'Active'"
+    if layers[layer]['query']:
+        query += ' AND ' + layers[layer]['query']
